32acc98dc83c8572a2aa95afaf863d7d709a7126
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2023-05-23 03:08:45 +0100

    fixed: remove client error

--- app/data/sql_scripts/ddl.sql
+++ app/data/sql_scripts/ddl.sql
@@ -5,20 +5,21 @@ create database if not exists reception;
 use reception;
 
 
--- user
+-- admin
 create table if not exists reception.admin (
-  id int not null auto_increment,
-  username varchar(20) not null unique,
-  password varchar(20) not null,
+  id int not null default 1,
+  username varchar(255) not null unique,
+  password varchar(255) not null,
   primary key(id)
 );
 
-TODO [0] : user have to have a name s the admin can reconize it in log
 -- user
-create table if not exists reception.user (
+create table if not exists reception.users (
   id int not null auto_increment,
-  username varchar(20) not null unique,
-  password varchar(20) not null,
+  first_name varchar(255) not null,
+  last_name varchar(255) not null,
+  username varchar(255) not null unique,
+  password varchar(255) not null,
   primary key(id)
 );
 
@@ -36,12 +37,11 @@ create table if not exists reception.client (
 -- archive
 create table if not exists reception.archive (
   id int not null auto_increment, -- pk
-  client_id int not null, -- fk
+  client_id int not null,
   name varchar(100),
   reception_date date,
   reception_time time,
   last_modification datetime,
-  foreign key(client_id) references client(id),
   primary key(id)
 );
 
